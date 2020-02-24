2885f92bf4dd93770046b2dab9801989e130583a
unknown <geralrcotrina@canvia.com>
2020-02-23 23:54:02 -0500

    v6

---
+++ prdts/js/productos.js
@@ -0,0 +1,40 @@
+
+
+window.onload = function() {
+	/*alert('ssss');*/
+};
+
+var ind_vog = false;
+
+function IndiceVO(){
+	if (ind_vog == false) {
+		IndiceV();
+	}else{
+		IndiceO();
+	}
+}
+
+
+function IndiceV() {
+	var ind_vo = document.getElementById('ind_vo');
+	var indice = document.getElementById('indice');
+	ind_vo.innerHTML ='<p onclick="IndiceVO();"><i class="fas fa-eye-slash h3"></i></p>';
+	ind_vo.classList.remove("infinite");
+	indice.classList.remove("indice");
+	indice.classList.add("indice_f");
+		ind_vog = true;
+}
+
+function IndiceO() {
+	var ind_vo = document.getElementById('ind_vo');
+	var indice = document.getElementById('indice');
+	ind_vo.innerHTML ='<p onclick="IndiceVO();"><i class="fas fa-list-ol h2"></i></p>';
+	ind_vo.classList.add("infinite");
+	indice.classList.remove("indice_f");
+	indice.classList.add("indice");
+		ind_vog = false;
+}
+
+ function NoBio(){
+ 	alert("Se encuentra en desarrollo.");
+ }
